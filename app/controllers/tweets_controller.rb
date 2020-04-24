52bff13c084425b6dca901a1a3a33b49345bc903
Alejandro <alejodiaz.123@gmail.com>
2020-04-23 22:06:39 -0400

    Dejando login y crear con una vista bonita

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,12 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /tweets
   # GET /tweets.json
   def index
     @tweets = Tweet.last(50)
+    @tweet = Tweet.new
   end
 
   # GET /tweets/1
@@ -14,7 +16,7 @@ class TweetsController < ApplicationController
 
   # GET /tweets/new
   def new
-    @tweet = Tweet.new
+    @tweet = current_user.tweets.build
   end
 
   # GET /tweets/1/edit
@@ -24,7 +26,7 @@ class TweetsController < ApplicationController
   # POST /tweets
   # POST /tweets.json
   def create
-    @tweet = Tweet.new(tweet_params)
+    @tweet = current_user.tweets.build(tweet_params)
 
     respond_to do |format|
       if @tweet.save
